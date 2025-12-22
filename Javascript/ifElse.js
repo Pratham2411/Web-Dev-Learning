5ae8aced82d7473b38003e668cece42e8fcba0d5
Pratham Raj <prathamraj2411@gmail.com>
2025-12-22 20:07:04 +0530

    Add files via upload

---
+++ Javascript/ifElse.js
@@ -0,0 +1,21 @@
+let age=21;
+if(age>=18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18+ to enter this side");
+}
+
+
+let haslicence=false;
+if(age>=16){
+    if(haslicence){
+        console.log("You can drive");
+    }
+    else{
+       console.log("You can't drive");
+    }
+}
+else{
+    console.log("You can't drive");
+}
