1e67e32a84d94baf0ebbf807d9d4e40a11e33733
Pratham Raj <prathamraj2411@gmail.com>
2025-12-22 14:26:09 +0530

    Add files via upload

---
+++ Javascript/circumfrance.js
@@ -0,0 +1,9 @@
+let radius;
+const pi = 3.14159;
+
+document.getElementById("mySubmit").onclick = function () {
+    radius = Number(document.getElementById("myText").value);
+    let circumference = 2 * pi * radius;
+    document.getElementById("myh2").textContent =
+        `Circumference = ${circumference}`;
+};
