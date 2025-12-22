6587500c453f96f454fce87a3ad2c60e506895be
Pratham Raj <prathamraj2411@gmail.com>
2025-12-22 11:41:47 +0530

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,5 @@
+console.log(`Hello`);
+// window.alert(`This is an alert`);
+document.getElementById("myh1").textContent=`Hello`;
+document.getElementById("myP").textContent=`This is a paragraph`;
+
