7d178759ae94d8daec62bc963e357f6f9e78dd77
Pratham Raj <prathamraj2411@gmail.com>
2025-12-22 21:01:37 +0530

    Add files via upload

---
+++ Javascript/switch.js
@@ -0,0 +1,31 @@
+let dayNumber = 3; // change this value from 1 to 7
+
+let dayName;
+
+switch (dayNumber) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day number";
+}
+
+console.log(`Day is: ${dayName}`);
