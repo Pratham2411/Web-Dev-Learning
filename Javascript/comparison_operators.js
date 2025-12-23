7588f11aa384f155ccc005e1d96de570408e7d25
Pratham Raj <prathamraj2411@gmail.com>
2025-12-23 14:53:21 +0530

    Add files via upload

---
+++ Javascript/comparison_operators.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = "5";
+
+// =  assignment (stores value)
+let x = 10;
+console.log(x);
+
+// ==  compares value only
+console.log(a == b);   // true
+
+// === compares value + type
+console.log(a === b);  // false
+
+// !=  compares value only (not equal)
+console.log(a != b);   // false
+
+// !== compares value + type (not equal)
+console.log(a !== b);  // true
