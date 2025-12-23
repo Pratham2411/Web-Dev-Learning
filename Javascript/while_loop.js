5bb238eefb3caed8b93848c33850aa9d09b5e502
Pratham Raj <prathamraj2411@gmail.com>
2025-12-23 15:09:32 +0530

    Implement login loop for username and password

--- Javascript/while_loop.js
+++ Javascript/while_loop.js
@@ -1,16 +1,18 @@
 let correctUsername = "Pratham";
 let correctPassword = "2411";
 
-let username = window.prompt("Enter username:");
-
-if (username === correctUsername) {
-    let password = window.prompt("Enter password:");
-
-    if (password === correctPassword) {
-        console.log("Login successful");
-    } else {
+let loggedIn = false;
+ let username;
+ let password;
+while (!loggedIn) {
+    username = window.prompt("Enter username:");
+    password = window.prompt("Enter password:");
+    if (username === correctUsername && password===correctPassword) {
+        loggedIn = true;
+       console.log("Login successful");
+        
+    }
+    else{
         console.log("Invalid credentials");
     }
-} else {
-    console.log("Invalid credentials");
 }
