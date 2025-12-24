713ad23437f2e1ee1c0a8701448c0826121f362d
Pratham Raj <prathamraj2411@gmail.com>
2025-12-24 14:37:47 +0530

    Add comments to the roll function
    Added comments to improve code readability.

--- Mini-Projects/Dice_Roller/script.js
+++ Mini-Projects/Dice_Roller/script.js
@@ -8,11 +8,11 @@ function roll() {
 
     for (let i = 0; i < n; i++) {
         const v = Math.floor(Math.random() * 6) + 1;
-
+       // creating a new image element
         const img = document.createElement("img");
         img.src = `images/${v}.png`;
         img.alt = `Dice ${v}`;
-
+        // putting the image into dice container
         box.appendChild(img);
     }
 }
