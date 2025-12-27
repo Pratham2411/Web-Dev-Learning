1d629476c05e4a795d270c205e488f9239672b1f
Pratham Raj <prathamraj2411@gmail.com>
2025-12-27 16:04:03 +0530

    Add files via upload

---
+++ Javascript/Objects.js
@@ -0,0 +1,19 @@
+    // object={ 
+    // key:value,    seperated by ,
+    // function()
+    //}
+
+const person={
+    firstName:"Pratham",
+    lastName:"raj",
+    age:21,
+    isEmployed: false,
+    sayHello: ()=> console.log("Hello")
+}
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.isEmployed);
+person.sayHello();
+// person.firstName ="Pintu";
+// console.log(person.firstName);
