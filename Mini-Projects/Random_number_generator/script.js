fa54c813fddd7ba91416fc62c60e1e7f2cd463ae
Pratham Raj <prathamraj2411@gmail.com>
2026-01-02 21:42:24 +0530

    Add files via upload

---
+++ Mini-Projects/Random_number_generator/script.js
@@ -0,0 +1,10 @@
+const min = 1;
+const max = 50;
+
+const generateBtn = document.getElementById("generateBtn");
+const result = document.getElementById("result");
+
+generateBtn.onclick = function () {
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    result.textContent = `Result: ${randomNum}`;
+};
