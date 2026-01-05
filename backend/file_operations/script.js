82c40902798125a1aa26bbc305e93b4ec429f042
Pratham Raj <prathamraj2411@gmail.com>
2026-01-05 16:48:57 +0530

    Add files via upload

---
+++ backend/file_operations/script.js
@@ -0,0 +1,12 @@
+ const fs=require('fs')
+ fs.writeFile("hi.txt","Hello",function(err){
+    if(err) console.error(err);
+    else console.log("done");
+ })
+
+ fs.appendFile("hi.txt",",My name is Pratham",function(err){
+    if(err) console.error(err);
+    else console.log("done");
+ })
+
+  
