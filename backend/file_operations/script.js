8c3c279e7db39211211f68f0285a5ebc13831564
Pratham Raj <prathamraj2411@gmail.com>
2026-01-05 17:09:12 +0530

    Add files via upload

--- backend/file_operations/script.js
+++ backend/file_operations/script.js
@@ -1,15 +1,19 @@
  const fs=require('fs')
- fs.writeFile("hi.txt","Hello",function(err){
-    if(err) console.error(err);
-    else console.log("done");
- })
+//  fs.writeFile("hi.txt","Hello",function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+//  })
 
- fs.appendFile("hi.txt",",My name is Pratham",function(err){
-    if(err) console.error(err);
-    else console.log("done");
- })
+//  fs.appendFile("hi.txt",",My name is Pratham",function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+//  })
 
-  fs.rename("hi.txt","hello.txt",function(err){
+//   fs.rename("hi.txt","hello.txt",function(err){
+//      if(err) console.error(err);
+//     else console.log("done");
+//   })
+  fs.copyFile("hello.txt","./copy/chacha.txt",function(err){
      if(err) console.error(err);
-    else console.log("done");
+    else console.log("copied");
   })
