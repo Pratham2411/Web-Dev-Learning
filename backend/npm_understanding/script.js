06dc9b1388d78c4ccb0d1a42e4303de7e2d9937a
Pratham Raj <prathamraj2411@gmail.com>
2026-01-07 22:06:26 +0530

    Add files via upload

---
+++ backend/npm_understanding/script.js
@@ -0,0 +1,8 @@
+//npm - node packae manager
+
+
+// install -> npm i one-liner-joke(optional)@verion
+// uninstall -> npm uninstall one-liner-joke
+// devdependencies:- packages used only till dvelopement not after deployment
+
+// scripts npm run script
