3343615663dc773184a001c228ede9ea8ebb302a
Pratham Raj <prathamraj2411@gmail.com>
2026-01-13 10:25:20 +0530

    Add files via upload

--- backend/form_handling/script.js
+++ backend/form_handling/script.js
@@ -3,5 +3,21 @@
 
 // npm i cookie-parser
 
+// data reading middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+ app.use(function(req,res,next){
+    console.log('middleware chala');
+    next();
+ });
+app.get("/",function(req,res){
+    res.send('Hello World')
+})
+app.get("/profile",function(req,res){
+    res.send('This is my profile ')
+})
+
+
+
+app.listen(3000)
