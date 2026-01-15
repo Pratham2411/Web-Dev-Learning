0af5cbcf0493e46305ac89abeaf3a3a486cb1586
Pratham Raj <prathamraj2411@gmail.com>
2026-01-15 12:17:10 +0530

    Add files via upload

---
+++ SQL/practice_qn1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS XYZ;
+
+CREATE TABLE IF NOT EXISTS  employee_info(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+salary INT NOT NULL);
+
+INSERT INTO employee_info
+ VALUES
+(1,"adam",25000),
+(2,"bob",30000),
+(3,"casey",40000);
+
+SELECT * FROM employee_info;
+
+
+
