d6157e4f45ba6c0316bae84f91eaba63203ff9fd
Pratham Raj <prathamraj2411@gmail.com>
2026-01-16 20:09:09 +0530

    Implement foreign keys and insert sample data
    Added foreign key constraints and sample data for dept and teacher tables.

--- SQL/foreign_keys.sql
+++ SQL/foreign_keys.sql
@@ -6,13 +6,40 @@ id INT PRIMARY KEY,
 name VARCHAR(50)
 );
 
+INSERT INTO dept
+VALUES
+(101, "english"),
+(102, "IT"); 
+
+SELECT * FROM dept;
 
 -- child table
-
 CREATE TABLE teacher (
 id INT PRIMARY KEY,
 name VARCHAR(50),
-dep_id INT ,
+dept_id INT ,
 FOREIGN KEY (dept_id) REFERENCES dept(id)
+-- Cascading
+ON UPDATE CASCADE
+ON DELETE CASCADE
 ) ;
+
+INSERT INTO teacher
+VALUES
+(101, "Adam", 101),
+(102, "Eve", 102);
+
+SELECT * FROM teacher;
+
+UPDATE dept
+SET id = 103
+WHERE id = 102; 
+
+
+
+
+
+
+
+
  
