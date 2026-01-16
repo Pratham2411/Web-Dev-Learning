7e6ea2a8b568a324b8e9db460ca7f018aed44308
Pratham Raj <prathamraj2411@gmail.com>
2026-01-16 10:18:18 +0530

    Add files via upload

--- SQL/select_command.sql
+++ SQL/select_command.sql
@@ -74,6 +74,19 @@ SELECT city, avg(marks)
 FROM student
 GROUP BY city;
 
+-- Having Clause
+-- Similar to Where i.e. applies some condition on rows.
+-- Used when we want to apply any condition after grouping.
+
+-- Count number of students in each city where max marks cross 90.
+
+SELECT city,count(name)
+FROM student
+GROUP BY city
+HAVING max(marks) > 90;
+
+
+
 
 
 
