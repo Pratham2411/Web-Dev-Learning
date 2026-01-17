e8120eaa2cd85815c3638fa3d793bbd498726b33
Pratham Raj <prathamraj2411@gmail.com>
2026-01-17 18:40:28 +0530

    Add files via upload

---
+++ SQL/views.sql
@@ -0,0 +1,13 @@
+
+-- MySQL Views
+-- A view is a virtual table based on the result-set of an SQL statement.
+-- syntax:-
+-- CREATE VIEW view1 AS
+-- SELECT rollno, name FROM student;
+-- SELECT * FROM view1;
+
+create view view1 as
+select rollno,full_name,marks
+from students;
+
+select* from view1;
