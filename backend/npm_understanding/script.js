941cc6cf85977ef3c9818d61601a8cc7e7eea235
Pratham Raj <prathamraj2411@gmail.com>
2026-02-11 23:10:58 +0530

    Delete backend/npm_understanding directory

--- backend/npm_understanding/script.js
+++
@@ -1,8 +0,0 @@
-//npm - node packae manager
-
-
-// install -> npm i one-liner-joke(optional)@verion
-// uninstall -> npm uninstall one-liner-joke
-// devdependencies:- packages used only till dvelopement not after deployment
-
-// scripts npm run script
