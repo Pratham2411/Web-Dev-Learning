956b75aa493202d8b0814e8145d0f697339024b9
Pratham Raj <prathamraj2411@gmail.com>
2026-02-13 12:20:17 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const app=require("./src/app")
+app.listen(3000,()=>{
+   console.log("server is running on port 3000");
 
-app.get("/",(req,res)=>{
-   res.send("Hello, response sent !");
-})
+}); // start server on port 3000
 
-app.get("/about",(req,res)=>{
-   res.send("Welcome to About Page");
-})
+// app.get("/",(req,res)=>{
+//    res.send("Hello, response sent !");
+// })
 
-app.listen(3000); // start server on port 3000
+// app.get("/about",(req,res)=>{
+//    res.send("Welcome to About Page");
+// })
+
+
 
 
 // node server.js start run
